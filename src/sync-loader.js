8d6c50225809c7ee24091948550999f43fff59c5
Giovanni Mendes Mansueto <topogigiovanni@gmail.com>
2017-03-29 00:10:58 -0300

    build plugin

--- src/sync-loader.js
+++ src/sync-loader.js
@@ -3,42 +3,73 @@
 
 	'use strict';
 
-	var $body;
+	var $document;
 	var $scripts;
 
 	var phase = {
 		number: 1,
-		loadedCount: null,
-		toLoadCount: null,
+		loadedCount: 0,
+		toLoadCount: 0,
 		started: false,
 		finished: false
 	};
 
 	function checkPhaseCompleted() {
-		console.log('cloaded', arguments);
+		phase.loadedCount++;
+		if(phase.loadedCount === phase.toLoadCount){
+			finishPhase();
+		}
 	}
 
 	function bindScriptOnLoad() {
-		$scripts.on('load', checkPhaseCompleted);
+		$scripts.on('load', function() {
+			$(this).addClass('sync-loaded');
+			checkPhaseCompleted();
+		});
 	}
 
-	function run() {
-		$scripts.each(function() {
-			var src = $(this).data('sync-loader');
-			$(this).attr('src', src);
+	function trigger(number) {
+		$document.trigger('syncloader.phase.' + number);
+	}
+
+	function finishPhase() {
+		trigger(phase.number);
+		phase.finished = true;
+		startPhase(++phase.number);
+	}
+
+	function startPhase(phaseNumber) {
+		var $toLoaded = $scripts.filter('[data-phase="' + phaseNumber + '"]').not('.sync-loaded');
+
+		$.extend(phase, {
+			number: phaseNumber,
+			loadedCount: 0,
+			toLoadCount: $toLoaded.length,
+			started: true,
+			finished: false
 		});
+
+		if(!phase.toLoadCount){
+			// end execution
+			phase.finished = true;
+			return;
+		}
+
+		$toLoaded
+			.each(function() {
+				$(this).attr('src', $(this).data('sync-loader'));
+			});
 	}
 
 	function init() {
 
 		// define vars
-		$body = $('body');
+		$document = $(document);
 		$scripts = $('[data-sync-loader]');
-		console.debug($scripts);
 
 		// login
 		bindScriptOnLoad();
-		run();
+		startPhase(1);
 
 	}
 
